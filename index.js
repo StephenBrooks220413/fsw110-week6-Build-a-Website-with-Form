8912a2f8620e9e049a3788d6acfd28b7728ce920
stephenbrooks40 <stephenbrooks714@gmail.com>
2021-06-12 15:54:21 -0700

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+function price1() {
+    document.getElementById("img1").innerHTML = "$39.99";
+}
+
+function price2() {
+    document.getElementById("img2").innerHTML = "$39.99";
+}
+
+function price3() {
+    document.getElementById("img3").innerHTML = "$39.99";
+}
+
+function Price1() {
+    document.getElementById("img1").innerHTML = "Your Price";
+}
+
+function Price2() {
+    document.getElementById("img2").innerHTML = "Your Price";
+}
+
+function Price3() {
+    document.getElementById("img3").innerHTML = "Your Price";
+}
